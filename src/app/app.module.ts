ffab1599ac6d4afe831da186dc92354a1cd05752
Simon grahn <simon.grahn@gmail.com>
2019-03-13 15:08:24 +0100

    kjkj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TrafficlightComponent } from './trafficlight/trafficlight.component';
 import { BooktravelComponent } from './booktravel/booktravel.component';
+import { TravelComponent } from './booktravel/travel/travel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TrafficlightComponent,
-    BooktravelComponent
+    BooktravelComponent,
+    TravelComponent
   ],
   imports: [
     BrowserModule,
